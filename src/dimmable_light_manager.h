5342eaebad81b7f73520aa517601aa91d645385a
Fabiano Riccardi <fabiuz4@hotmail.it>
2018-12-26 16:27:35 +0100

    Change license to LGPL 2.1, as Arduino Core
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- src/dimmable_light_manager.h
+++ src/dimmable_light_manager.h
@@ -1,27 +1,17 @@
 /***************************************************************************
  *   Copyright (C) 2018 by Fabiano Riccardi                                *
  *                                                                         *
- *   This file is part of Dimmable Light for ESP8266.                      *
+ *   This file is part of Dimmable Light for ESP.                          *
  *                                                                         *
- *   Dimmable Light for Arduino is free software; you can redistribute     *
- *   it and/or modify it under the terms of the GNU General Public         *
+ *   Dimmable Light for ESP is free software; you can redistribute         *
+ *   it and/or modify it under the terms of the GNU Lesser General Public  *
  *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
+ *   version 2.1 of the License, or (at your option) any later version.    *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
+ *   This library is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   As a special exception, if other files instantiate templates or use   *
- *   macros or inline functions from this file, or you compile this file   *
- *   and link it with other works to produce a work based on this file,    *
- *   this file does not by itself cause the resulting work to be covered   *
- *   by the GNU General Public License. However the source code for this   *
- *   file must still be made available in accordance with the GNU General  *
- *   Public License. This exception does not invalidate any other reasons  *
- *   why a work based on this file might be covered by the GNU General     *
- *   Public License.                                                       *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
