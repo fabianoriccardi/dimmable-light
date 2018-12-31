cb3fb3cfef1a97e24bc24fa54a8d660d48536d31
Fabiano Riccardi <fabiuz4@hotmail.it>
2018-12-31 16:47:32 +0100

    License
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- src/hw_timer_avr.cpp
+++ src/hw_timer_avr.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2018 by Fabiano Riccardi                                *
+ *                                                                         *
+ *   This file is part of Dimmable Light for ESP.                          *
+ *                                                                         *
+ *   Dimmable Light for ESP is free software; you can redistribute         *
+ *   it and/or modify it under the terms of the GNU Lesser General Public  *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
 #ifdef AVR
 
 #include <util/atomic.h>
