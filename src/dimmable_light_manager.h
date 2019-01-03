9f03d86deca99586efb729d059d098dea08f4aad
Fabiano Riccardi <fabiuz4@hotmail.it>
2019-01-03 18:01:18 +0100

    license update
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- src/dimmable_light_manager.h
+++ src/dimmable_light_manager.h
@@ -1,9 +1,9 @@
 /***************************************************************************
- *   Copyright (C) 2018 by Fabiano Riccardi                                *
+ *   Copyright (C) 2018, 2019 by Fabiano Riccardi                          *
  *                                                                         *
- *   This file is part of Dimmable Light for ESP.                          *
+ *   This file is part of Dimmable Light for Arduino                       *
  *                                                                         *
- *   Dimmable Light for ESP is free software; you can redistribute         *
+ *   Dimmable Light for Arduino is free software; you can redistribute     *
  *   it and/or modify it under the terms of the GNU Lesser General Public  *
  *   License as published by the Free Software Foundation; either          *
  *   version 2.1 of the License, or (at your option) any later version.    *
