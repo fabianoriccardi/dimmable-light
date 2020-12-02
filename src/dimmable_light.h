c66f562e4a0b45b03d7346784e63bb06f444c827
Jean Gauthier <gauthier_j100@hotmail.com>
2020-12-02 12:07:28 -0500

    Update dimmable_light.h
    Added new functions
    - setSyncDir(uint8_t dir) to adjust the interrupt direction FALLING, RISING, CHANGE
    - setSyncPullup(bool pullup) to activate internal pull-up resistor on zero-crossing detection
    - setGateTurnOffTime(uint16_t timeOff) to change the value of the gate turn off time

--- src/dimmable_light.h
+++ src/dimmable_light.h
@@ -112,8 +112,29 @@ public:
   /**
    * Set the pin dedicated to receive the AC zero cross signal.
    */
-  static void setSyncPin(uint8_t pin){
-    Thyristor::setSyncPin(pin);
+  static uint8_t setSyncPin(uint8_t pin){
+    return Thyristor::setSyncPin(pin);
+  }
+  
+  /**
+   * Set the pin direction (FALLING, RISING, CHANGE)
+   */
+  static uint8_t setSyncDir(uint8_t dir){
+    return Thyristor::setSyncDir(dir);
+  }
+  
+  /**
+   * Set the pin direction (FALLING, RISING, CHANGE)
+   */
+  static bool setSyncPullup(bool pullup){
+    return Thyristor::setSyncPullup(pullup);
+  }
+  
+  /**
+   * Set the gate turn off time
+   */
+  static bool setGateTurnOffTime(uint16_t turnOffTime){
+    return Thyristor::setGateTurnOffTime(turnOffTime);
   }
 
   /**
